6c8904bda84c73bb8cd688248111844ff4cd06fc
zzinis <lizzyin@naver.com>
2023-07-23 23:35:19 +0900

    message ui

--- todos/src/App.js
+++ todos/src/App.js
@@ -4,6 +4,15 @@ import './App.css';
 function App() {
   return (
     <>
+      <div className='wrapper'>
+        <div className='toast toast-success'>
+          <main className='toast__message'>
+            <header className='toast__message-title'>Success</header>
+            <p className='toast__message-text'>Right on!</p>
+          </main>
+          <button className='toast__button'>Dismiss</button>
+        </div>
+      </div>
     </>
   );
 }
